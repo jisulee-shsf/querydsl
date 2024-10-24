c427a670930070d5fd9db63ca40bc9aa4f6aa967
Jisu Lee <jisulee.shsf@gmail.com>
2024-10-24 21:04:53 +0900

    기본 문법 - 조인(on절 - 연관관계가 없는 엔티티 외부 조인)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -252,9 +252,30 @@ public class QuerydslBasicTest {
         List<Tuple> result = queryFactory
                 .select(member, team)
                 .from(member)
-                .join(member.team, team)
+                .leftJoin(member.team, team)
                 .on(team.name.eq("teamA"))
 //                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * on절을 활용한 연관관계가 없는 엔티티 외부 조인
+     * 멤버 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
                 .fetch();
 
         for (Tuple tuple : result) {
